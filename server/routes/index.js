6044f86f4dda05bb4d2d90c047cc9a8ae61de1f3
emresandikci <me@emresandikci.com>
2020-04-28 15:41:37 +0300

    refactor: nextjs routing logic changed

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,20 @@
 const routes = require('next-routes');
+const urls = require('./urls');
+const routeDefinitions = routes();
 
-module.exports = routes()
-  .add({ name: 'home', pattern: '/', page: 'public/home' })
-  .add({ name: 'about', pattern: '/about', page: 'public/about' });
+let routeNames = {};
+
+Object.entries(urls).forEach(function ([key, value]) {
+  routeDefinitions.add({ name: value.name, pattern: value.pattern, page: value.page });
+  Object.defineProperty(routeNames, key, {
+    value: {
+      name: value.name,
+      path: value.pattern,
+      absoluteUrl: `${process.env.EB_BASE_ROUTE}${value.pattern}`,
+    },
+  });
+});
+
+routeDefinitions.Routes = routeNames;
+
+module.exports = routeDefinitions;
---
+++ server/routes/urls.js
@@ -0,0 +1,6 @@
+ const urls = { 
+  home:{ name: 'home', pattern: '/', page: 'public/home' },
+  about:{ name: 'about', pattern: '/about', page: 'public/about' }
+};
+
+module.exports = urls;
