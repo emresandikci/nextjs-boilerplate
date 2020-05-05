fe757540070679b98b5b2bd4d5381af2848a1247
emresandikci <me@emresandikci.com>
2020-05-05 03:38:09 +0300

    chore: local ip access added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const next = require('next');
 const routes = require('./routes');
 const express = require('express');
+const os =require('os');
 
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handler = routes.getRequestHandler(app);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.prepare().then(() => {
   const server = express();
@@ -19,5 +20,11 @@ app.prepare().then(() => {
   server.listen(PORT, error => {
     if (error) throw error;
     console.log(`>${process.env.NODE_ENV} mode Ready on http://localhost:${PORT}`);
+    console.log(`>${process.env.NODE_ENV} mode Ready on http://localhost:${PORT} || http://${getLocalExternalIP()}:${PORT}`);
   });
 });
+
+
+const getLocalExternalIP = () => [].concat(...Object.values(os.networkInterfaces()))
+  .filter(details => details.family === 'IPv4' && !details.internal)
+  .pop().address
